bd937699ded3412846706fab170f2c0a1e401fe6
Marina Oreskovic <44814229+marinaoreskovic@users.noreply.github.com>
2024-06-06 22:56:32 -0300

    Update services text.tsx

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -11,7 +11,7 @@ const ServicesPage = () => {
                     <div className="max-w-5xl text-center">
                         <div className="mb-10">
                             <h1 className="text-2xl leading-tight md:text-4xl md:mb-5">Mis <span className="font-bold text-secondary">servicios.</span></h1>
-                            <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de Desarrollo Web Front-end especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías como: HTML5, CSS3, JavaScript, React.js, diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
+                            <p className="mb-3 text-xl text-gray-300">Ofrezco servicios de Desarrollo Web Front-end especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías como HTML5, CSS3, JavaScript y React diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
                         </div>
 
                         {/* SLIDER */}
