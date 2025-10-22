9946553ef2d66e5ef04c4e8e9223379852f2596c
Marina Oreskovic <44814229+marinaoreskovic@users.noreply.github.com>
2025-10-22 16:43:55 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 "use client"
-
-import { CoverParticles } from "@/components/cover-particles";
 import Introduction from "@/components/introduction";
 import TransitionPage from "@/components/transition-page";
 
@@ -9,7 +7,6 @@ export default function Home() {
     <main>
       <TransitionPage />
       <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
-        <CoverParticles />
         <Introduction />
       </div>
     </main>
