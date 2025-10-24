e9c05f946fc20a68e4aaacfd308d1e8c7d8a7298
Marina Oreskovic <44814229+marinaoreskovic@users.noreply.github.com>
2025-10-24 18:43:56 -0300

    Update data.tsx

--- data.tsx
+++ data.tsx
@@ -75,7 +75,7 @@ export const dataAboutPage = [
     title: "Full Stack Developer",
     subtitle: "Addalitica",
     description:
-      "Trabajo en un equipo multidisciplinario para clientes del sector financiero como NBCH, Finning y Credicomer. Mi rol es el desarrollo de nuevas funcionalidades, migraciones y la resolución de problemas técnicos en sus plataformas de Solicitude de créditos. Me involucro tanto en la parte Front-end como en el Back-end.",
+      "Trabajo en un equipo multidisciplinario para clientes del sector financiero como NBCH, Finning y Credicomer. Mi rol es el desarrollo de nuevas funcionalidades, migraciones y la resolución de problemas técnicos en sus plataformas de Solicitudes de Créditos. Me involucro tanto en la parte Front-end como en el Back-end.",
     date: "2024",
   },
   {
@@ -230,44 +230,9 @@ export const dataPortfolio = [
 export const dataTestimonials = [
   {
     id: 1,
-    name: "George Snow",
+    name: "",
     description:
-      "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
+      "",
     imageUrl: "/profile1.png",
-  },
-  {
-    id: 2,
-    name: "Juan Pérez",
-    description:
-      "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
-    imageUrl: "/profile2.png",
-  },
-  {
-    id: 3,
-    name: "María García",
-    description:
-      "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
-    imageUrl: "/profile3.png",
-  },
-  {
-    id: 4,
-    name: "Laura Snow",
-    description:
-      "¡Qué descubrimiento tan fantástico! Los testimonios aquí son honestos y detallados. Me siento más seguro al tomar decisiones después de leer las experiencias compartidas por otros usuarios.",
-    imageUrl: "/profile4.png",
-  },
-  {
-    id: 5,
-    name: "Carlos Sánchez",
-    description:
-      "Una joya en la web. Los testimonios son fáciles de encontrar y están bien organizados. ¡Definitivamente mi destino número uno cuando necesito referencias confiables!",
-    imageUrl: "/profile5.png",
-  },
-  {
-    id: 6,
-    name: "Antonio Martínez",
-    description:
-      "¡Fantástico recurso para aquellos que buscan validación antes de tomar decisiones importantes! Los testimonios aquí son veraces y realmente útiles. ¡Gracias por simplificar mi proceso de toma de decisiones!",
-    imageUrl: "/profile6.png",
   },
 ];
